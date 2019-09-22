352f6b4f4c4bb7dc3e3347c11b353554c10b5613
Gaurav-Chandorkar <gaurav.chandorkar@gmail.com>
2019-09-22 22:53:48 +0530

    working app, add glide

--- app/src/main/java/com/grv/glammtest/adapter/RestaurantAdapter.kt
+++ app/src/main/java/com/grv/glammtest/adapter/RestaurantAdapter.kt
@@ -1,13 +1,19 @@
 package com.grv.glammtest.adapter
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import com.grv.glammtest.R
 import com.grv.glammtest.TouchActionDelegates
 import com.grv.glammtest.database.RestaurantEntity
 import kotlinx.android.synthetic.main.item_restaurant.view.*
+import java.net.URI
+import java.net.URL
 
 class RestaurantAdapter(
     var restaurantList: MutableList<RestaurantEntity>,
@@ -15,7 +21,7 @@ class RestaurantAdapter(
 ) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-
+    val TAG = "RestaurantAdapter"
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
         RestaurantViewHolder(
             LayoutInflater.from(parent.context).inflate(
@@ -42,13 +48,16 @@ class RestaurantAdapter(
     }
 
     inner class RestaurantViewHolder(var view: View) : RecyclerView.ViewHolder(view) {
+        val requestOptions = RequestOptions().diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
 
         fun setData(position: Int) {
-            var restaurant=restaurantList.get(position)
+            var restaurant = restaurantList.get(position)
             view.img_thumb
-            view.tv_hotel_name.text=restaurant.name
-            view.tv_address.text=restaurant.address
+            view.tv_hotel_name.text = restaurant.name
+            view.tv_address.text = restaurant.address
 
+            Log.e(TAG, "url ${restaurant.thumb}")
+            Glide.with(view).load(URL(restaurant.thumb)).apply(requestOptions).into(view.img_thumb)
         }
     }
 
