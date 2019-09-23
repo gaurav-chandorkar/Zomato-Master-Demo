e9d5738856cb6ece312dbb87f2536ccda39b2400
Gaurav-Chandorkar <gaurav.chandorkar@gmail.com>
2019-09-23 23:08:11 +0530

    final commit

--- app/src/main/java/com/grv/glammtest/model/BaseModel.kt
+++ app/src/main/java/com/grv/glammtest/model/BaseModel.kt
@@ -7,7 +7,7 @@ import java.lang.Exception
 const val TIMEOUT_DURATION_MILLIS = 3000L
 
 open class BaseModel {
-
+    // single function to perform operation in background,
      suspend fun performOperationWithTimeOut(function: () -> Unit, callback: SuccessCallback) {
           var job= GlobalScope.async {
 
